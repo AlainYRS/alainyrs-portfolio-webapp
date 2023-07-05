abf2b157788f582a63fd267880fe24cc5e13a213
AlainYRS <galain0106@gmail.com>
2023-07-05 07:49:31 -0400

    Styles

--- app/page.tsx
+++ app/page.tsx
@@ -9,30 +9,31 @@ export default function Home() {
           <div className="fixed bottom-0 left-0 flex items-end justify-center w-full h-48 bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
             <a
               className="flex gap-2 p-8 pointer-events-none place-items-center lg:pointer-events-auto lg:p-0"
-              href="#"
+              href="https://github.com/AlainYRS/alainyrs-portfolio-webapp"
               target="_blank"
               rel="noopener noreferrer"
             >
               My{' '}
               <Image
-                src="/github.webp"
-                alt="Vercel Logo"
+                src="/icons/Git.webp"
+                alt="Git Logo"
                 className="dark:invert"
-                width={100}
-                height={24}
+                width={21}
+                height={21}
                 priority
-              />
+                />
+              repo.
             </a>
           </div>
         </div>
 
         <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
           <Image
-            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
+            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] rounded-3xl"
+            src="/Images/AYRS.jpeg"
+            alt="Profile Image"
+            width={140}
+            height={35}
             priority
           />
         </div>
@@ -56,11 +57,9 @@ export default function Home() {
             </p>
           </a>
 
-          <a
-            href="#"
+          <Link
+            href="/skills"
             className="px-5 py-4 transition-colors border border-transparent rounded-lg group hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-            target="_blank"
-            rel="noopener noreferrer"
           >
             <h2 className={`mb-3 text-2xl font-semibold`}>
               Skills{' '}
@@ -71,13 +70,11 @@ export default function Home() {
             <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
               What I have learned.
             </p>
-          </a>
+          </Link>
           
-          <a
+          <Link
             href="#"
             className="px-5 py-4 transition-colors border border-transparent rounded-lg group hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
           >
             <h2 className={`mb-3 text-2xl font-semibold`}>
               Experience{' '}
@@ -88,14 +85,12 @@ export default function Home() {
             <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
               Where I have worked.
             </p>
-          </a>
+          </Link>
 
 
-          <a
+          <Link
             href="#"
             className="px-5 py-4 transition-colors border border-transparent rounded-lg group hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
           >
             <h2 className={`mb-3 text-2xl font-semibold`}>
               About{' '}
@@ -106,7 +101,7 @@ export default function Home() {
             <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
               My journey.
             </p>
-          </a>
+          </Link>
         </div>
       </main>
     </>
