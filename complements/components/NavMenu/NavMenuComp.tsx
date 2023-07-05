c6e87fc86994a5fcb2026f352c993ca74a08b589
AlainYRS <galain0106@gmail.com>
2023-07-05 16:19:00 -0400

    Styles

--- complements/components/NavMenu/NavMenuComp.tsx
+++ complements/components/NavMenu/NavMenuComp.tsx
@@ -1,49 +1,52 @@
+'use client';
 import CompStyles from './NavMenuComp.module.css';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
 
 export default function NavMenuComp(){
+    const [DisplayMenu, setDisplayMenu] = useState(false);
+
     return(
         <>
             <div className={(CompStyles.NavMenu)}>
-                <a id={CompStyles.MenuIcon} className={(CompStyles.MenuIcon)}>
-                    <Image className={(CompStyles.IconImg)} src="/Icons/MenuIcon.png" fill alt=""/>
-                </a>
                 <div className={(CompStyles.DivMenu)}>
-                    <label className={(CompStyles.LabelMenu)} htmlFor="HomeMenu">Home</label>
-                    <Link href="/"   id="HomeMenu" className={(CompStyles.MenuIcon)}>
-                        <Image className={(CompStyles.IconImg)} src="/Icons/Home.png" fill alt=""/>
-                    </Link>
-                </div>
-                <div className={(CompStyles.DivMenu)}>
-                    <label className={(CompStyles.LabelMenu)} htmlFor="SkillsMenu">Skills</label>
-                    <div id={CompStyles.SkillsMenu} className={(CompStyles.MenuIcon)}>
-                        <Image src="/Icons/Skills.png" fill alt=""/>
+                    <div id={CompStyles.BurgerIcon} className={(CompStyles.MenuIcon)} onClick={()=>setDisplayMenu(!DisplayMenu)}>
+                        <Image className={(CompStyles.IconImg)} src="/Icons/MenuIcon.png" fill alt=""/>
                     </div>
-                </div>
-                <div className={(CompStyles.DivMenu)}>
-                    <label className={(CompStyles.LabelMenu)} htmlFor="JobExpMenu">Jobs</label>
-                    <div  id={CompStyles.JobExpMenu} className={(CompStyles.MenuIcon)}>
-                        <Image src="/Icons/JobExperience.png" fill alt=""/>
-                    </div>
-                </div>
-                <div className={(CompStyles.DivMenu)}>
-                    <label className={(CompStyles.LabelMenu)} htmlFor="AcademicMenu">Education</label>
-                    <div  id={CompStyles.AcademicMenu} className={(CompStyles.MenuIcon)}>
-                        <Image src="/Icons/Academic.png" fill alt=""/>
-                    </div>
-                </div>
-                <div className={(CompStyles.DivMenu)}>
-                    <label className={(CompStyles.LabelMenu)} htmlFor="PortfolioMenu">Portfolio</label>
-                    <div id={CompStyles.PortfolioMenu} className={(CompStyles.MenuIcon)}>
-                        <Image src="/Icons/Portfolio.png" fill alt=""/>
-                    </div>
-                </div>
-                <div className={(CompStyles.DivMenu)}>
-                    <label className={(CompStyles.LabelMenu)} htmlFor="GitRepoMenu">Git</label>
-                    <div id={CompStyles.GitRepoMenu} className={(CompStyles.MenuIcon)}>
-                        <Image src="/Icons/Git.webp" fill alt=""/>
-                    </div>
+                    {DisplayMenu &&
+                        <>
+                            <Link href="/" id="HomeMenu" className={(CompStyles.MenuIcon)}>
+                                <Image className={(CompStyles.IconImg)} src="/Icons/Home.png" fill alt=""/>
+                            </Link>
+                            <label className={(CompStyles.LabelMenu)} htmlFor="HomeMenu">Home</label>
+
+                            <Link href="/skills" id="SkillsMenu" className={(CompStyles.MenuIcon)}>
+                                <Image src="/Icons/Skills.png" fill alt=""/>
+                            </Link>
+                            <label className={(CompStyles.LabelMenu)} htmlFor="SkillsMenu">Skills</label>
+
+                            <Link href="/jobs" id="JobExpMenu" className={(CompStyles.MenuIcon)}>
+                                <Image src="/Icons/JobExperience.png" fill alt=""/>
+                            </Link>
+                            <label className={(CompStyles.LabelMenu)} htmlFor="JobExpMenu">Jobs</label>
+
+                            <Link href="/education" id="AcademicMenu" className={(CompStyles.MenuIcon)}>
+                                <Image src="/Icons/Academic.png" fill alt=""/>
+                            </Link>
+                            <label className={(CompStyles.LabelMenu)} htmlFor="AcademicMenu">Education</label>
+
+                            <Link href="/portfolio" id="PortfolioMenu" className={(CompStyles.MenuIcon)}>
+                                <Image src="/Icons/Portfolio.png" fill alt=""/>
+                            </Link>
+                            <label className={(CompStyles.LabelMenu)} htmlFor="PortfolioMenu">Portfolio</label>
+
+                            <Link href="/git" id="GitRepoMenu" className={(CompStyles.MenuIcon)}>
+                                <Image src="/Icons/Git.webp" fill alt=""/>
+                            </Link>
+                            <label className={(CompStyles.LabelMenu)} htmlFor="GitRepoMenu">Git</label>
+                        </>
+                    }
                 </div>
             </div>
         </>
